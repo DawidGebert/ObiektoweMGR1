84d4c85dc9458c60e7bfc259eb2f7c7a1e607012
DawidGebert <dawid.gebert@gmail.com>
2024-12-11 20:42:38 +0100

    User Manager model
    create user and superuser methods

--- Authorization/AuthorizationAPI/authentication/models.py
+++ Authorization/AuthorizationAPI/authentication/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def createUser(self, username,email,password=None):
+        if username is None:
+            raise TypeError('Users should have a username')
+        if email is None:
+            raise TypeError('Users should have a email')
+        
+        user=self.model(username=username,email=self._normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def createSuperuser(self, username,email,password=None):
+        if password is None:
+            raise TypeError('Password should not be None')
+        
+        user=self.createUser(username,email,password)
+        user.is_superuser = True
+        user.save()
+        return user
+    
+
