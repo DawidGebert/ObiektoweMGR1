0d230ed1b140ad42c3e9b9762a422af58c12ce6d
DawidGebert <dawid.gebert@gmail.com>
2024-12-13 00:01:30 +0100

    Swagger docs, working user creation with token via email

--- Authorization/AuthorizationAPI/AuthorizationAPI/settings.py
+++ Authorization/AuthorizationAPI/AuthorizationAPI/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'authentication'
+    'authentication',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
@@ -132,8 +134,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_USER_TLS=True
+EMAIL_USE_TLS = True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
--- Authorization/AuthorizationAPI/authentication/models.py
+++ Authorization/AuthorizationAPI/authentication/models.py
@@ -3,18 +3,18 @@ from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, Permi
 # Create your models here.
 
 class UserManager(BaseUserManager):
-    def createUser(self, username,email,password=None):
+    def create_user(self, username,email,password=None):
         if username is None:
             raise TypeError('Users should have a username')
         if email is None:
             raise TypeError('Users should have a email')
         
-        user=self.model(username=username,email=self._normalize_email(email))
+        user=self.model(username=username,email=self.normalize_email(email))
         user.set_password(password)
         user.save()
         return user
 
-    def createSuperuser(self, username,email,password=None):
+    def create_superuser(self, username,email,password=None):
         if password is None:
             raise TypeError('Password should not be None')
         
